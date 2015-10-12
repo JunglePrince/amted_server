ac31ff45ff1d887d9bbc88ef486c9deb8ea302f1
Eric Zeng <ericzeng2@gmail.com>
2015-10-11 22:34:27 -0700

    Implement multi-command forking, pipes
    - Start by forking the main shell process, child runs runCommand
    - First child starts at the last command, forks a process for the
      preceding command, and execs its command
    - Fork processes in reverse order until first command is reached

--- shell.cc
+++ shell.cc
@@ -70,7 +70,7 @@ char** tokenize(char* input, const char* delimiter) {
  * Takes a line of shell input and splits it into an array of arrays containing
  * the commands and their command line arguments.
  */
-char*** parseCommands(char* input) {
+char*** parseCommands(char* input, int* len) {
   char** rawCommands = tokenize(input, "|");
   if (rawCommands == NULL)
     return NULL;
@@ -98,53 +98,97 @@ char*** parseCommands(char* input) {
   }
 
   free(rawCommands);
+  *len = count;
   return commands;
 }
 
-void runCommands(char*** commands) {
-  pid_t pid = fork();
-  if (pid == 0) {
-    // Child process
-    char** argv = commands[0];
-    int err = execvp(*argv, argv);
-    if (err == -1) {
-      cout << "execvp() error\n";
+/**
+ * Sets up pipes for the preceding command in the array, forks another shell
+ * process to run the preceding command, and then execs the command at index.
+ */
+int runCommands(char*** commands, int index) {
+  // If this is the first command, all of the pipes are already set up,
+  // and we just need to exec the command.
+  if (index == 0) {
+    char** argv = commands[index];
+    if (execvp(*argv, argv) == -1) {
+      cout << "execvp() error:\n";
       printErrno();
-    }
-  } else {
-    // Parent process
-    pid_t finished;
-    int err = wait(&finished);
-    if (err == -1) {
-      cout << "wait() error\n";
-      printErrno();
-      return;
+      return -1;
     }
   }
+
+  // Create pipes to receive data from the preceding command
+  int pipes[2];  // 0 is read, 1 is write
+  if (pipe(pipes) == -1) {
+    cout << "pipe() error:\n";
+    printErrno();
+    return -1;
+  };
+
+  pid_t pid = fork();
+
+  if (pid == 0) {
+    // Child process
+    // Set stdout (for the preceding process) to the write end of the pipe.
+    dup2(pipes[1], STDOUT_FILENO);
+    // Close the read end, since it is not used.
+    close(pipes[0]);
+    // Run the preceding command
+    return runCommands(commands, index - 1);
+  } else {
+    // Parent process
+    // Set stdin to the read end of the pipe. stdout has either been set by
+    // the next command in the chain, or is just stdout for the last command.
+    dup2(pipes[0], STDIN_FILENO);
+    // Close the write end, since it is not used.
+    close(pipes[1]);
+    char** argv = commands[index];
+    if (execvp(*argv, argv) == -1) {
+      cout << "execvp() error:\n";
+      printErrno();
+      return -1;
+    }
+  }
+  return 0;
 }
 
 int main(int argc, char* argv[]) {
   cout << "$ ";
-
   // Read a line from stdin
   char* line = NULL;
   size_t len = 0;
   int read;
   while ((read = getline(&line, &len, stdin)) != -1) {
     line[read - 1] = '\0'; // Remove newline
-    char*** commands = parseCommands(line);
-
+    int len;
+    char*** commands = parseCommands(line, &len);
     if (commands) {
-      // Execute commands
-      runCommands(commands);
+      pid_t pid = fork();
+      if (pid == 0) {
+        // Child process runs the commands
+        if (runCommands(commands, len - 1) == -1) {
+          return 1;
+        }
+      } else {
+        // Parent process waits for all commands to return
+        pid_t finished;
+        int err = wait(&finished);
 
-      // Free allocated memory for commands
-      int i = 0;
-      while (commands[i]) {
-	free(commands[i]);
-	i++;
+        // Free memory allocated for the commands
+        int i;
+        for (i = 0; i < len; i++) {
+          free(commands[i]);
+        }
+        free(commands);
+
+        // Exit on error
+        if (err == -1) {
+          cout << "wait() error\n";
+          printErrno();
+          break;
+        }
       }
-      free(commands);
     }
     cout << "$ ";
   }
