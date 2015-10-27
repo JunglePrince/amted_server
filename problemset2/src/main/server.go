496ee1c74b3ec4658fe1738c9f9b5634afba7d78
Nicholas Shahan <nick.shahan@gmail.com>
2015-10-27 15:14:36 -0700

    added me index to command line args

--- problemset2/src/main/server.go
+++ problemset2/src/main/server.go
@@ -1,11 +1,28 @@
 package main
 
-import "os"
+import "fmt"
 import "lockservice"
+import "os"
+import "strconv"
 
 func main() {
-	servers := os.Args[1:]
-	me := 0
+	if len(os.Args) <= 2 {
+		printUsage()
+		return
+	}
+
+	servers := os.Args[1 : len(os.Args)-1]
+	me, err := strconv.Atoi(os.Args[len(os.Args)-1])
+
+	if err != nil {
+		printUsage()
+		fmt.Printf("ERROR: Last argument must be an index that identifies this server.\n")
+		return
+	}
 
 	lockservice.MakeLockService(servers, me)
+}
+
+func printUsage() {
+	fmt.Printf("Usage: server.go <ServerIP:Port> ... <ServerIP:Port> <Zero based \"me\" index>\n")
 }
